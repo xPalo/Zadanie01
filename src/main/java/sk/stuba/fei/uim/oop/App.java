44e68a3750c128181c58931703b650b309625848
xPalo <xpalo@stuba.sk>
2021-02-16 17:27:46 +0100

    Commit 03 - typy

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -5,5 +5,11 @@ public class App
     public static void main(String[] args)
     {
         System.out.println("Ola world");
+        int cislo = 12;
+        double realne = 1.5;
+        char znak = 'A';
+        String retazec = "Java";
+
+        System.out.println("Java = " + cislo);
     }
 }
