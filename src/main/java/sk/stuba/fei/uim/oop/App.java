3f9fce5329998821baf7f72da5271790e6e651cb
xPalo <xpalo@stuba.sk>
2021-02-16 17:41:26 +0100

    Commit 04 - funkcia

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -1,15 +1,19 @@
 package sk.stuba.fei.uim.oop;
 
-public class App
-{
+public class App {
     public static void main(String[] args)
     {
-        System.out.println("Ola world");
+
+        //System.out.println("Ola world");
+
         int cislo = 12;
         double realne = 1.5;
-        char znak = 'A';
-        String retazec = "Java";
 
-        System.out.println("Java = " + cislo);
+        System.out.println("12 + 1.5 = " + scitaj(cislo, realne));
+    }
+
+    public static double scitaj(int a, double b)
+    {
+        return a + b;
     }
 }
